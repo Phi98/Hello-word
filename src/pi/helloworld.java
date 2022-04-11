dc5db9625e1a3d62bd0a6d51706104322cffc182
Le Bao Phi <lebaophi3103@gmail.com>
2022-04-11 20:33:02 +0700

    Object and data interaction

--- src/pi/helloworld.java
+++ src/pi/helloworld.java
@@ -1,7 +1,23 @@
 package pi;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class helloworld {
-public static void main(String[] args) {
-System.out.print("Hello world");
-}
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+        Student st1 = new Student("Le Bao Phi",20,"Quang Tri");
+        Student st2 = new Student("Nguyen Minh Vu",20,"Quang Tri");
+        Student st3 = new Student("Le Thuy Trang",20,"Quang Tri");
+        
+        List<Student> sts = new ArrayList<Student>();
+        sts.add(st1);
+        sts.add(st2);
+        sts.add(st3);
+        
+        for(Student st : sts) {
+        	  System.out.println(st.toString());
+        	}
+        
+	}
 }
